7c43eea5d3e6aa39a1288e27cf10332a2daa392e
Marco Parra <marcoparra311@gmail.com>
2022-08-28 13:35:08 -0400

    Mostrar varios tipos de datos declarados del componente usando typeScript

--- AprendiendoAngular/src/app/app.component.ts
+++ AprendiendoAngular/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  users = ['ryan', 'joe', 'cameron'];
+  name: string = 'Marco';
+  age: number;
+  address: {
+    street: string,
+    city: string
+  }
+  hobbies: string[];
+  constructor () {
+    this.age = 28;
+    this.address = {
+      street: 'AV 2-C',
+      city: 'Maracaibo'
+    }
+    this.hobbies = ['pareja', 'estudiar', 'jugar'];  
+  }  
 }
--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { HolaMundoComponent } from './components/hola-mundo/hola-mundo.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PeliculasComponent,
-    HolaMundoComponent
+    HolaMundoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
