0490be0208dfcc9d955722ac1c13c90ea530eada
Marco Parra <marcoparra311@gmail.com>
2022-09-03 15:40:18 -0400

    Routing básico

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import  {RouterModule, Route} from  '@angular/router';
 //Components
 import { AppComponent } from './app.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { HolaMundoComponent } from './components/hola-mundo/hola-mundo.component';
 import { UserComponent } from './user/user.component';
+import { AboutComponent } from './components/about/about.component';
 //Forms
 import { FormsModule } from '@angular/forms';
 //Services
 import { DataService } from './data.service';
 import { HttpClientModule } from '@angular/common/http';
+
+const routes: Route[] = [
+  {path: '', component: HolaMundoComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
@@ -21,7 +28,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
 
   ],
   providers: [DataService],
